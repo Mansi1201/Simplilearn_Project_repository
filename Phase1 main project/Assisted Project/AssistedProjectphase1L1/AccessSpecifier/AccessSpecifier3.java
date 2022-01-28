fb0faee7f58e0b1ffc16c007cc730581ae175686
Mansi1201 <97694818+Mansi1201@users.noreply.github.com>
2022-01-28 10:21:44 +0530

    Add files via upload

---
+++ Phase1 main project/Assisted Project/AssistedProjectphase1L1/AccessSpecifier/AccessSpecifier3.java
@@ -0,0 +1,12 @@
+package com.sl.Assignments.AssistedProjectphase1L1.AccessSpecifier;
+
+import com.sl.Assignments.AssistedProjectphase1L1.pack1.proaccessspecifiers;
+
+
+public class AccessSpecifier3 extends proaccessspecifiers{
+	public static void main(String[] args) {
+		AccessSpecifier3 obj = new AccessSpecifier3 (); 
+		 obj.display(); 
+		}
+
+}
